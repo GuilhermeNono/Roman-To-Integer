d8e5ba5f2625774288c2e2e612a345feef5bb706
Guilherme Nono <guilherme.nono@uppercase.com.br>
2024-04-24 14:40:08 -0300

    Finishing Exercise

---
+++ RomanToInteger/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using RomanToInteger;
+
+bool isInLoop = true;
+
+while (isInLoop)
+{
+    Console.Write("- ");
+    var teste = Console.ReadLine();
+    if (teste.Equals("Exit", StringComparison.CurrentCultureIgnoreCase))
+    {
+        isInLoop = false;
+        continue;
+    }
+    Console.WriteLine("> {0}", new Solution().RomanToInt(teste));
+}
---
+++ RomanToInteger/Solution.cs
@@ -0,0 +1,78 @@
+﻿namespace RomanToInteger;
+
+public class Solution
+{
+    public int RomanToInt(string s)
+    {
+        Dictionary<char, int> romanDigits = new Dictionary<char, int>
+        {
+            { 'I', 1 },
+            { 'V', 5 },
+            { 'X', 10 },
+            { 'L', 50 },
+            { 'C', 100 },
+            { 'D', 500 },
+            { 'M', 1000 }
+        };
+        char[] romanArray = s.ToCharArray();
+        int result = 0;
+
+        for (var i = 0; i < romanArray.Length; i++)
+        {
+            if (i != 0)
+            {
+                if (romanArray[i].ToString().Equals("V") &&
+                    romanArray[i - 1].ToString().Equals("I"))
+                {
+                    result -= romanDigits['I'];
+                    result += romanDigits['V'] - romanDigits['I'];
+                    continue;
+                }
+
+                if (romanArray[i].ToString().Equals("X") &&
+                    romanArray[i - 1].ToString().Equals("I"))
+                {
+                    result -= romanDigits['I'];
+                    result += romanDigits['X'] - romanDigits['I'];
+                    continue;
+                }
+
+                if (romanArray[i].ToString().Equals("L") &&
+                    romanArray[i - 1].ToString().Equals("X"))
+                {
+                    result -= romanDigits['X'];
+                    result += romanDigits['L'] - romanDigits['X'];
+                    continue;
+                }
+                
+                if (romanArray[i].ToString().Equals("C") &&
+                    romanArray[i - 1].ToString().Equals("X"))
+                {
+                    result -= romanDigits['X'];
+                    result += romanDigits['C'] - romanDigits['X'];
+                    continue;
+                }
+                
+                if (romanArray[i].ToString().Equals("D") &&
+                    romanArray[i - 1].ToString().Equals("C"))
+                {
+                    result -= romanDigits['C'];
+                    result += romanDigits['D'] - romanDigits['C'];
+                    continue;
+                }
+                
+                if (romanArray[i].ToString().Equals("M") &&
+                    romanArray[i - 1].ToString().Equals("C"))
+                {
+                    result -= romanDigits['C'];
+                    result += romanDigits['M'] - romanDigits['C'];
+                    continue;
+                }
+            }
+
+            result += romanDigits[romanArray[i]];
+        }
+
+        return result;
+    }
+}
